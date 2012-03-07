adc4d9af92cc10d89b2ce1a36a319637b5b5ffdf
Takafumi Arakaki <aka.tkf@gmail.com>
2012-03-08 00:57:12 +0100

    Added header comments

--- auto-complete-rst.el
+++ auto-complete-rst.el
@@ -1,3 +1,38 @@
+;;; auto-complete-rst.el --- Auto-complete extension for ReST and Sphinx
+;;
+;; Filename: auto-complete-rst.el
+;; Description: Auto-complete extension for ReST and Sphinx
+;; Author: ARAKAKI, Takafumi
+;; Maintainer: ARAKAKI, Takafumi
+;; Created: Sun Nov 27 17:48:44 2011 +0100
+;; URL: https://github.com/tkf/auto-complete-rst
+;;
+;; This file is NOT part of GNU Emacs.
+
+;;; License:
+;;
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 3, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+;; Floor, Boston, MA 02110-1301, USA.
+
+;;; Commentary:
+
+;; Usage:
+;;    (eval-after-load "rst" '(auto-complete-rst-init))
+
+;;; Code:
+
 (eval-when-compile
   (require 'cl))
 
